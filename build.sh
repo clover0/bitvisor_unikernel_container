cc85d806b070644ac485665370292e1fc7ec145a
koenuma <-->
2020-08-25 17:56:28 +0900

    integrate build process for macOS

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 
 set -eu
 
-docker-compose run --rm ubuntu1604 sh -c "make && make -C boot/loader && make -C boot/uefi-loader"
+docker-compose run --rm ubuntu1804 sh -c "make && make -C boot/loader && make -C boot/uefi-loader"
